1f1393658ed2dda446a57a72ab1a1f6b627f083f
Fredrik Zander Kloster <fredrik.kloster93@gmail.com>
2025-12-02 20:23:20 +0100

    part 2 complete

--- 2025/day-2-gift-shop/id_cracker.rs
+++ 2025/day-2-gift-shop/id_cracker.rs
@@ -17,15 +17,21 @@ impl IdCracker {
 
         for current_id in self.first_id..=self.last_id {
             let id_as_string = current_id.to_string();
-            if id_as_string.len() % 2 != 0 {
-                continue;
-            }
 
-            let (first_chunk, second_chunk) = self.split_id(id_as_string.as_str());
+            // Part 1:
+            // if id_as_string.len() % 2 != 0 {
+            //     continue;
+            // }
+            // let (first_chunk, second_chunk) = self.split_id(id_as_string.as_str());
+            //
+            // if first_chunk == second_chunk {
+            //     valid_ids.push(current_id);
+            // }
 
-            if first_chunk == second_chunk {
+            // Part 2
+            if self.id_has_valid_split(id_as_string.as_str()) {
                 valid_ids.push(current_id);
-            }
+            };
         }
 
         valid_ids
@@ -34,5 +40,37 @@ impl IdCracker {
     fn split_id<'a>(&self, id: &'a str) -> (&'a str, &'a str) {
         let split_location = id.len() / 2;
         id.split_at(split_location)
+    }
+
+    fn id_has_valid_split(&self, id: &str) -> bool {
+        for chunk_count in 1..=id.len() {
+            if id.len() % chunk_count != 0 || chunk_count == 1 {
+                continue;
+            }
+
+            let chunk_size = id.len() / chunk_count;
+            let chunks: Vec<&str> = id
+                .char_indices()
+                .step_by(chunk_size)
+                .map(|(i, _)| &id[i..i + chunk_size])
+                .collect();
+
+            if self.check_all_chuks_equal(chunks) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    fn check_all_chuks_equal(&self, chunks: Vec<&str>) -> bool {
+        if chunks.is_empty() || chunks.len() == 1 {
+            return false;
+        }
+        for chunk in chunks.iter() {
+            if chunk != &chunks[0] {
+                return false;
+            }
+        }
+        true
     }
 }
