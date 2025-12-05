5869e5aadd5fad1c8367176d40325e8989f09a2e
Fredrik Zander Kloster <fredrik.kloster93@gmail.com>
2025-12-05 16:18:26 +0100

    Day 5 Complete!
    So good!!!

--- 2025/day-5-cafeteria/fresh_list.rs
+++ 2025/day-5-cafeteria/fresh_list.rs
@@ -30,29 +30,58 @@ impl FreshList {
 
     pub fn get_valid_ingredients_count(&mut self) -> u64 {
         self.adjust_overlapping_ranges();
-        let total_ids: u64 = self
-            .ranges
+        self.ranges
             .iter()
             .filter(|range| range.end >= range.start)
             .map(|range| range.end - range.start + 1)
-            .sum();
-        total_ids
+            .sum()
     }
 
     pub fn adjust_overlapping_ranges(&mut self) {
+        let mut perfect_list: Vec<Range> = Vec::new();
+
         self.ranges.sort_by(|a, b| a.start.cmp(&b.start));
         for i in 0..self.ranges.len() {
-            if i > 0 && self.ranges[i].start <= self.ranges[i - 1].end {
-                self.ranges[i].start = self.ranges[i - 1].end + 1;
+            let mut current = self.clone_range(i);
+
+            if perfect_list.is_empty() {
+                perfect_list.push(current);
+                continue;
             }
 
-            if i == self.ranges.len() - 1 {
-                break;
+            let previous = perfect_list.last().unwrap();
+
+            if current.start <= previous.end {
+                current.start = previous.end + 1;
+
+                if current.end <= previous.start {
+                    continue;
+                }
             }
 
-            if self.ranges[i].end > self.ranges[i + 1].start {
-                self.ranges[i].end = self.ranges[i + 1].start - 1;
-            }
+            perfect_list.push(current);
+        }
+
+        self.ranges = perfect_list;
+    }
+
+    pub fn get_ranges(&self) -> Vec<(u64, u64)> {
+        self.ranges
+            .iter()
+            .map(|range| (range.start, range.end))
+            .collect()
+    }
+
+    pub fn print_ranges(&self) {
+        for range in &self.ranges {
+            println!("{}-{})", range.start, range.end);
+        }
+    }
+
+    fn clone_range(&self, index: usize) -> Range {
+        Range {
+            start: self.ranges[index].start,
+            end: self.ranges[index].end,
         }
     }
 }
